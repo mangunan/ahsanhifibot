f33c028ecf7880736f2ae54801073078f8a9e9df
mangunan <subkhi06icha@gmail.com>
2019-07-13 04:19:17 +0700

    Create setting.py

---
+++ setting.py
@@ -0,0 +1,4 @@
+username = "subhiahsan@gmail.com" #Your Deezer email
+password = "cyber4glte" #Your Deezer password
+token = "756279689:AAFQ6_1qDlS7qNv-_P1tT2KIGDbh-omK_Vk" #Your Telegram bot token
+deezer_token = "248cb04f336e012f0f8074cd78595d5cba88eb802b1b0ad7428f99f8a73e01bf7364a30478a5fc0faacc5769f50bf3be9432affcfb737349e825cb0f772ec524f8b318bf7bf8b92c27cd6d292e18830279463e2f49a29cec73c86ac2410024fc" #Your Deezer token log (for get it log in Deezer from your PC press f12 and find arl cookie)
